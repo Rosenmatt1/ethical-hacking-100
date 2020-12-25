7042712f9d1088478568e5e602d0a3c810d34fc5
Matthew Rosenberg <rosenmatt1@gmail.com>
2020-12-24 23:23:22 -0700

    Added another file

---
+++ test.py
@@ -0,0 +1,7 @@
+print("test")
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0 x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0 
