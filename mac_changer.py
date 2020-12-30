e32775a3ce652026a1f21652903079c028f9fcd7
Matthew Rosenberg <rosenmatt1@gmail.com>
2020-12-29 17:02:40 -0700

    Updated script to use variables in script

--- mac_changer.py
+++ mac_changer.py
@@ -2,4 +2,11 @@
 
 import subprocess
 
-subprocess.call("ifconfig", shell=True)  # the true allows to run Linux commands through function
+interface = "wlan0"
+new_mac = "00:1:22:33:44:77"
+
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+subprocess.call("ifconfig " + interface " down", shell=True)  # the true allows to run Linux commands through function
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig " + interface + "up", shell=True)
--- shabang.py
+++ shabang.py
@@ -3,5 +3,5 @@
 print("hello world!")
 
 ifconfig eth0 down
-ifconfig eth0 hw 00:11:22:33:44:55
+ifconfig eth0 hw ether 00:11:22:33:44:55
 ifconfig eth0 up
